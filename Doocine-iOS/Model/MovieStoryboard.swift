00593751c20c2abd2a0f650b62cbe3e5ddac7a6f
SOHN DAI GUEN <nhos126@gmail.com>
2017-03-18 15:54:59 +0900

    make splash view controller

---
+++ Doocine-iOS/Model/MovieStoryboard.swift
@@ -0,0 +1,15 @@
+//
+//  MovieStoryboard.swift
+//  Doocine-iOS
+//
+//  Created by Sohn on 18/03/2017.
+//  Copyright © 2017 Sohn. All rights reserved.
+//
+
+import Foundation
+
+class MovieStoryboard {
+    init() {
+        
+    }
+}
