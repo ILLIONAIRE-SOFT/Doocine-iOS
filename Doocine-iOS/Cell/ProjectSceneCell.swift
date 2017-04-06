8098b7d5cd4066e685e168bd60d2e09029e723b3
SOHN DAI GUEN <nhos126@gmail.com>
2017-04-06 14:27:47 +0900

    make ProjectSceneCell

---
+++ Doocine-iOS/Cell/ProjectSceneCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProjectSceneCell.swift
+//  Doocine-iOS
+//
+//  Created by Sohn on 06/04/2017.
+//  Copyright © 2017 Sohn. All rights reserved.
+//
+
+import UIKit
+
+class ProjectSceneCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
