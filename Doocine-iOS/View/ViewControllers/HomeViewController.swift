2499799889e131d39bb00251eadee4d16ba6841f
SOHN DAI GUEN <nhos126@gmail.com>
2017-05-10 18:50:39 +0900

    add image

--- Doocine-iOS/View/ViewControllers/HomeViewController.swift
+++ Doocine-iOS/View/ViewControllers/HomeViewController.swift
@@ -79,7 +79,7 @@ class HomeViewController: BaseViewController {
         }
         
         self.collectionView.reloadData()
-        self.projectCountLabel.text = String(self.projects.count)
+        self.projectCountLabel.text = String(self.projects.count) + "projects"
     }
 }
 
